63aa92144488dabcb2fd8c396cbe9c2f029eb92b
dallasHome <doroshuk33@yandex.ru>
2020-01-13 18:18:27 +0300

    update gitignore for Netbeans

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    AppFirst
                 </div>
 
                 <div class="links">
